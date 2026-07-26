8c44ada8e0e400f2d23492d866e4e064a0fd2386
Benyaminiism <121764473+Benyaminiism@users.noreply.github.com>
2026-07-26 18:30:11 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if st.button("پردازش با Gemini"):
             try:
                 # فراخوانی مدل سریع gemini-2.5-flash
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-1.5-flash',
                     contents=prompt,
                 )
                 st.success("پردازش با موفقیت انجام شد:")
